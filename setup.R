5e17e042a55cc0bf9e77d02e3cd356f28b6dc99b
bsrose25 <37912671+bsrose25@users.noreply.github.com>
2022-05-18 10:17:53 -0500

    Initial commit
    20220518 CV

---
+++ setup.R
@@ -0,0 +1,8 @@
+datadrivencv::use_datadriven_cv(
+  full_name = "Bailey S. Rose",
+  data_location = "https://docs.google.com/spreadsheets/d/1GzWsjEmnRuA_jermG4XWFHqJ0DCcMk8L-nFlrLp-SgE/edit?usp=sharing",
+  pdf_location = "https://github.com/bsrose25/Rose-cv/rose_cv.pdf",
+  html_location = "https://github.com/bsrose25/Rose-cv/rose_cv.html",
+  source_location = "https://github.com/bsrose25/Rose-cv",
+  use_network_logo = TRUE
+)
